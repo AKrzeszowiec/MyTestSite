fc28c723ab343c7cd8662c48694ba81e17ad0080
amayor3.14 <amayor3.14@gmail.com>
2018-03-03 13:24:41 +0100

    Added ProductRepo

---
+++ MyTestSite/Repos/IDatabaseRepo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyTestSite.Repos
+{
+    interface IDatabaseRepo<T>
+    {
+        Task<List<T>> GetAllAsync();
+        Task<T> GetByIdAsync(int? id);
+        Task<int> AddAsync(T entity);
+        Task<int> AddRangeAsync(List<T> entities);
+        Task<int> SaveAsync(T entity);
+        Task<int> DeleteAsync(T entity);
+        Task<int> DeleteAsync(int id);
+
+    }
+}
---
+++ MyTestSite/Repos/ProductRepo.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MyTestSite.Models;
+using System.Data.Entity;
+using System.Threading.Tasks;
+
+namespace MyTestSite.Repos
+{
+    public class ProductRepo : IDatabaseRepo<Product>, IDisposable
+    {
+        ApplicationDbContext Context = new ApplicationDbContext();
+        DbSet<Product> Table;
+
+        public ProductRepo()
+        {
+            Table = Context.Products;
+        }
+
+        public Task<int> AddAsync(Product entity)
+        {
+            Table.Add(entity);
+            return Context.SaveChangesAsync();
+        }
+
+        public Task<int> AddRangeAsync(List<Product> entities)
+        {
+            Table.AddRange(entities);
+            return Context.SaveChangesAsync();
+        }
+
+        public Task<int> DeleteAsync(Product entity)
+        {
+            Context.Entry(entity).State = EntityState.Deleted;
+            return Context.SaveChangesAsync();
+        }
+
+        public Task<int> DeleteAsync(int id)
+        {
+            Context.Entry(new Product(){ProductId = id}).State = EntityState.Deleted;
+            return Context.SaveChangesAsync();
+        }
+
+        public Task<List<Product>> GetAllAsync()
+        {
+            return Table.ToListAsync();
+        }
+
+        public Task<Product> GetByIdAsync(int? id)
+        {
+            return Table.FindAsync(id);
+        }
+
+        public Task<int> SaveAsync(Product entity)
+        {
+            Context.Entry(entity).State = EntityState.Modified;
+            return Context.SaveChangesAsync();
+        }
+
+        #region IDisposable Support
+        private bool disposedValue = false; // To detect redundant calls
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    // TODO: dispose managed state (managed objects).
+                    Context.Dispose();
+                }
+
+                // TODO: free unmanaged resources (unmanaged objects) and override a finalizer below.
+                // TODO: set large fields to null.
+
+                disposedValue = true;
+            }
+        }
+
+        // TODO: override a finalizer only if Dispose(bool disposing) above has code to free unmanaged resources.
+        // ~ProductRepo() {
+        //   // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+        //   Dispose(false);
+        // }
+
+        // This code added to correctly implement the disposable pattern.
+        public void Dispose()
+        {
+            // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+            Dispose(true);
+            // TODO: uncomment the following line if the finalizer is overridden above.
+            // GC.SuppressFinalize(this);
+        }
+        #endregion
+    }
+}
